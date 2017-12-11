32b188e26a26ebc68de6045ebfe187d5ec652865
eduardo <ecmattos@ig.com.br>
2017-12-11 16:19:32 -0200

    Estrutura condicional 01

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ var app = new Vue({
 		],
 		activedView: 0,
 		bills: [
-			{due_date: '20/08/2016', name: 'Conta de luz', value: 70.99, done: 1},
+			{due_date: '20/08/2016', name: 'Conta de luz', value: 81.86, done: 1},
 			{due_date: '21/08/2016', name: 'Conta de água', value: 70.99, done: 0},
 			{due_date: '22/08/2016', name: 'Conta de telefone', value: 70.99, done: 0},
+			{due_date: '22/08/2016', name: 'Condomínio', value: 170.87, done: 0},
 			{due_date: '23/08/2016', name: 'Mercado', value: 70.99, done: 1},
-			{due_date: '24/08/2016', name: 'Gasolina', value: 70., done: 0}
+			{due_date: '24/08/2016', name: 'Gasolina', value: 45.45, done: 0}
 		]
 	},
 	computed: {
@@ -23,13 +24,13 @@ var app = new Vue({
 					count++;
 				}
 			}
-			return !count?"Nenhuma Conta a Pagar":"Existe(m) "+ count + " a Pagar";
+			return !count?"Nenhuma Conta a pagar":"Existe(m) " + count + " a pagar";
 		}
 	},
 	methods: {
 		showView: function(id){
 			console.log(id);
-			this.actived = id;
+			this.activedView = id;
 		}
 	}
 });
